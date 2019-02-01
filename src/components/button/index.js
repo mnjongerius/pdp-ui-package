8757d41a9fa6806b177f8d53abb5a69ad4bb1be2
Maarten Jongerius <maarten@touchtribe.nl>
2019-02-01 11:27:35 +0100

    Implement Storybook

--- src/components/button/index.js
+++ src/components/button/index.js
@@ -1,6 +1,13 @@
-export default ({as, children}) => {
+import React from 'react'
+
+const Button = ({as, children}) => {
     const Component = as
     return (
         <Component className="ui-button">{children}</Component>
     )
 }
+Button.defaultProps = {
+  as: 'div'
+}
+
+export default Button
---
+++ src/components/button/index.stories.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+import { storiesOf } from '@storybook/react'
+import Component from './index'
+
+storiesOf('Components|TODO', module)
+  .add('Default', () => (
+    <Component>
+      foo
+    </Component>
+  ))
