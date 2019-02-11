ecc0ba20d429d82ade5abb518975ad9d60133d10
Maarten Jongerius <maarten@touchtribe.nl>
2019-02-11 16:32:53 +0100

    Rewrite InputField to use React Hooks

--- src/components/input-field/index.js
+++ src/components/input-field/index.js
@@ -1,83 +1,43 @@
-import React from 'react'
+import React, { useState } from 'react'
 import PropTypes from 'prop-types'
 
-class InputField extends React.Component {
-  static propTypes = {
-    type: PropTypes.string,
-    value: PropTypes.string,
-    name: PropTypes.string.isRequired,
-    className: PropTypes.string,
-    onFocus: PropTypes.func,
-    onChange: PropTypes.func,
-    onBlur: PropTypes.func
-  }
-  static defaultProps = {
-    type: 'text',
-    className: 'c-input-field',
-    onFocus: () => {},
-    onChange: () => {},
-    onBlur: () => {}
-  }
-  state = {
-    value: this.props.value,
-    propsValue: this.props.value
+const InputField = (props) => {
+  const {onChange, value: propsValue, ...rest} = props
+  const [value, setValue] = useState(propsValue)
+  const [valueFromProps, setValueFromProps] = useState()
+
+  if (propsValue !== valueFromProps) {
+    setValue(propsValue)
+    setValueFromProps(propsValue)
   }
 
-  static getDerivedStateFromProps (props, state) {
-    const { value } = props
-    const { propsValue } = state
-    if (value !== propsValue) {
-      return {
-        ...state,
-        value,
-        propsValue: value
-      }
-    }
-    return {
-      ...state
-    }
-  }
-
-  render () {
-    const {
-      type,
-      name,
-      className,
-      ...props
-    } = this.props
-    const { value } = this.state
-    return (
-      <input
-        {...props}
-        type={type}
-        name={name}
-        className={className}
-        onFocus={this._onFocus}
-        onChange={this._onChange}
-        onBlur={this._onBlur}
-        value={value}
-      />
-    )
-  }
-
-  _onFocus = (evt) => {
-    const { onFocus } = this.props
-    onFocus(evt)
-  }
-
-  _onChange = (evt) => {
-    const { target: { value } } = evt
-    const { onChange } = this.props
-    this.setState({
-      value
-    })
-    onChange(evt)
-  }
-
-  _onBlur = (evt) => {
-    const { onBlur } = this.props
-    onBlur(evt)
-  }
+  return (
+    <input
+      {...rest}
+      onChange={evt => {
+        setValue(evt.target.value)
+        onChange(evt)
+      }}
+      value={value}
+    />
+  )
 }
+InputField.propTypes = {
+  type: PropTypes.string,
+  value: PropTypes.string,
+  name: PropTypes.string.isRequired,
+  className: PropTypes.string,
+  onFocus: PropTypes.func,
+  onChange: PropTypes.func,
+  onBlur: PropTypes.func
+}
+InputField.defaultProps = {
+  type: 'text',
+  className: 'c-input-field',
+  onFocus: () => {},
+  onChange: () => {},
+  onBlur: () => {}
+}
+
 
 export default InputField
