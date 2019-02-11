b7695f92cd5fa7c524badb479af673f85b690045
Maarten Jongerius <maarten@touchtribe.nl>
2019-02-11 13:02:36 +0100

    Add InputField component

---
+++ src/components/input-field/index.stories.js
@@ -0,0 +1,30 @@
+import React from 'react'
+
+import { storiesOf } from '@storybook/react'
+import InputField from './index'
+import './styles/index.scss'
+
+const onFocus = function onFocus () {
+}
+
+const onChange = function onChange () {
+}
+
+const onBlur = function onBlur () {
+}
+
+storiesOf('InputField', module)
+  .addParameters({
+    info: {
+      text: `Component to render a input-field`
+    }
+  })
+  .add('Default', () => (
+      <InputField
+        value="Default value"
+        onFocus={onFocus}
+        onChange={onChange}
+        onBlur={onBlur}
+      />
+    )
+  )
